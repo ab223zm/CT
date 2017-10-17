e4c115266461edf16d9bcf188e7a4a82a9fe136f
ab223zm <alexbjaremo@gmail.com>
2017-10-17 16:57:13 +0200

    Create Lab6Task2.c

---
+++ LAb6/Lab6Task2.c
@@ -0,0 +1,62 @@
+
+//Weird error regarding type being 6 not 2
+#include<avr/io.h>
+#include<stdio.h>
+#include <stdlib.h>
+//#define BAUD 2400
+//#define UBRR_VAL 47
+
+void uart_int(void);
+void uart_trans(unsigned char data);
+
+int main(void){
+	uart_int();
+	char towrite[100];
+	char* temp ="\rAO0001Pizza                 is";
+	int i;
+	int checksum=0;
+	for (i=0;i<34;i++){
+		checksum+=temp[i];
+	}
+	
+	checksum=checksum%256;
+	
+	sprintf(towrite,"%s%02X\n", temp, checksum);
+	
+	for(i=0;i<37;i++){
+			uart_trans(towrite[i]);
+		}
+	
+	
+	int checksum1=0;
+	
+	temp="\rBO001life";
+	for (i=0;i<11;i++){
+		checksum1+=temp[i];
+	}
+	
+	checksum1=checksum1%256;
+	
+	sprintf(towrite,"%s%02X\n", temp, checksum1);
+	
+	for(i=0;i<14;i++){
+		uart_trans(towrite[i]);
+	}
+	temp ="\rZD0013C\n";
+	
+	for(i=0;i<9;i++){
+		uart_trans(temp[i]);
+	}
+	
+	return 0;
+}
+
+void uart_int(void){
+	UBRR1L=25;
+	UCSR1B =(1<<TXEN1) | (1<<RXEN1);
+	
+}
+void uart_trans(unsigned char data){
+	while(!(UCSR1A & (1<<UDRE1)));
+	UDR1 = data;
+}
