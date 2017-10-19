82fdee0903d3a0a8cc6003845fec8076f5a1eaae
ab223zm <alexbjaremo@gmail.com>
2017-10-19 21:14:27 +0200

    Update Lab6Task2.c

--- LAb6/Lab6Task2.c
+++ LAb6/Lab6Task2.c
@@ -12,42 +12,27 @@ void uart_trans(unsigned char data);
 int main(void){
 	uart_int();
 	char towrite[100];
-	char* temp ="\rAO0001Pizza                 is";
+	char* temp ="\rAO0001Pizza1234567890123456789is";
 	int i;
+	
+	//first two lines
 	int checksum=0;
-	for (i=0;i<34;i++){
-		checksum+=temp[i];
-	}
-	
+	for (i=0;i<34;i++){checksum+=temp[i];}
 	checksum=checksum%256;
-	
 	sprintf(towrite,"%s%02X\n", temp, checksum);
+	for(i=0;i<37;i++){uart_trans(towrite[i]);}
 	
-	for(i=0;i<37;i++){
-			uart_trans(towrite[i]);
-		}
-	
-	
-	int checksum1=0;
-	
+	//last line
+	checksum=0;
 	temp="\rBO001life";
-	for (i=0;i<11;i++){
-		checksum1+=temp[i];
-	}
-	
-	checksum1=checksum1%256;
-	
+	for (i=0;i<12;i++){checksum+=temp[i];}
+	checksum=checksum%256;
 	sprintf(towrite,"%s%02X\n", temp, checksum1);
 	
-	for(i=0;i<14;i++){
-		uart_trans(towrite[i]);
-	}
+	//**********
+	for(i=0;i<14;i++){uart_trans(towrite[i]);}
 	temp ="\rZD0013C\n";
-	
-	for(i=0;i<9;i++){
-		uart_trans(temp[i]);
-	}
-	
+	for(i=0;i<9;i++){uart_trans(temp[i]);}
 	return 0;
 }
 
