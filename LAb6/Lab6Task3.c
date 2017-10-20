5b76eb7a4a4b74e5f39bed7003e51814d420fa0c
ab223zm <alexbjaremo@gmail.com>
2017-10-20 15:41:48 +0200

    Update Lab6Task3.c

--- LAb6/Lab6Task3.c
+++ LAb6/Lab6Task3.c
@@ -1,6 +1,9 @@
 #include<avr/io.h>
 #include<stdio.h>
 #include <stdlib.h>
+#define F_CPU 1830000UL
+#include <util/delay.h>
+
 //#define BAUD 2400
 //#define UBRR_VAL 47
 
@@ -9,29 +12,54 @@ void uart_trans(unsigned char data);
 
 int main(void){
 	uart_int();
-	char towrite[300]; 
-  
-	char* temp ="\rAO0001Pizza1234567890123456789is";
+	char towrite[200];
+	char* temp ="\rAO0001Computer Science,201712345678Computer Technology";
 	int i;
-	
-	//first two lines
+
 	int checksum=0;
-	for (i=0;i<34;i++){checksum+=temp[i];}
+	for (i=0;i<55;i++){
+		checksum+=temp[i];
+		}
 	checksum=checksum%256;
 	sprintf(towrite,"%s%02X\n", temp, checksum);
-	for(i=0;i<37;i++){uart_trans(towrite[i]);}
-	
-	//last line
+	for(i=0;i<58;i++){
+		uart_trans(towrite[i]);
+		}
+		
 	checksum=0;
-	temp="\rBO001life";
-	for (i=0;i<12;i++){checksum+=temp[i];}
+	temp="\rBO0001Assignment#6";
+	for (i=0;i<20;i++){
+		checksum+=temp[i];
+	}
 	checksum=checksum%256;
-	sprintf(towrite,"%s%02X\n", temp, checksum1);
-	for(i=0;i<15;i++){uart_trans(towrite[i]);}
+	sprintf(towrite,"%s%02X\n", temp, checksum);
+	for(i=0;i<23;i++){
+		uart_trans(towrite[i]);
+	}
+	
+	temp ="\rZD0013C\n";
+	for(i=0;i<9;i++){
+		uart_trans(temp[i]);
+	}
+		
+	_delay_ms(5000);
+
+	checksum=0;
+	temp="\rBO0001Ruth and Alex";
+	for (i=0;i<21;i++){
+		checksum+=temp[i];
+		}
+	checksum=checksum%256;
+	sprintf(towrite,"%s%02X\n", temp, checksum);
+	for(i=0;i<24;i++){
+		uart_trans(towrite[i]);
+		}
 	
 	//*********
 	temp ="\rZD0013C\n";
-	for(i=0;i<9;i++){uart_trans(temp[i]);}
+	for(i=0;i<9;i++){
+		uart_trans(temp[i]);
+		}
 	return 0;
 }
 
