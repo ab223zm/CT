c824e84adb2d82b9f0cbc05111c7365f1ee09bad
ab223zm <alexbjaremo@gmail.com>
2017-11-01 10:55:00 +0100

    Update Lab6Task2.c

--- LAb6/Lab6Task2.c
+++ LAb6/Lab6Task2.c
@@ -1,4 +1,28 @@
-
+/*;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 1DT301, Computer Technology I
+; Date: 2017-10-17
+; Author:
+; Student name 1: Ruth Dirnfeld
+; Student name 2: Alexandra Bjäremo
+;
+; Lab number: 6
+; Title: CyberTech Wall Display
+;
+; Hardware: STK600, CPU ATmega2560
+;
+; Function: Displaying a character on the CyberTech Wall Display
+;
+; Input ports: TX, RX on PIND2, respective PIND3.
+;
+; Output ports: CyberTech Wall Display connected to serial port RS232
+;
+; Subroutines: Initialization and transmission routines for the display
+; Included files: m2560def.inc
+;
+; Other information: Clock set at 1,83MHz
+;
+; Changes in program: None.
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 #include<avr/io.h>
 #include<stdio.h>
 #include <stdlib.h>
@@ -37,19 +61,20 @@ int main(void){
 		uart_trans(towrite[i]);
 		}
 	
-	//*********
+	//default command for broadcasting 
 	temp ="\rZD0013C\n";
 	for(i=0;i<9;i++){
 		uart_trans(temp[i]);
 		}
 	return 0;
 }
-
+//initialization function
 void uart_int(void){
 	UBRR1L=25;
 	UCSR1B =(1<<TXEN1) | (1<<RXEN1);
 	
 }
+//transmission function
 void uart_trans(unsigned char data){
 	while(!(UCSR1A & (1<<UDRE1)));
 	UDR1 = data;
