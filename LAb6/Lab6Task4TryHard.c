996b8994f6d3d41536cd66a9998cf9c8f7b54fe3
ab223zm <alexbjaremo@gmail.com>
2017-11-01 20:30:00 +0100

    Update Lab6Task4TryHard.c

--- LAb6/Lab6Task4TryHard.c
+++ LAb6/Lab6Task4TryHard.c
@@ -1,6 +1,6 @@
 /*;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ; 1DT301, Computer Technology I
-; Date: 2017-10-17
+; Date: 2017-10-28
 ; Author:
 ; Student name 1: Ruth Dirnfeld
 ; Student name 2: Alexandra Bjäremo
@@ -10,8 +10,8 @@
 ;
 ; Hardware: STK600, CPU ATmega2560
 ;
-; Function: Displaying characters on all the CyberTech Wall Display lines
-; and changing the last line after 5 seconds.
+; Function: Retrieving and displaying characters from Putty
+; on the selected line of the CyberTech Wall Display. 
 ;
 ; Input ports: TX, RX on PIND2, respective PIND3.
 ;
@@ -22,7 +22,7 @@
 ;
 ; Other information: Clock set at 1,83MHz
 ;
-; Changes in program: None.
+; Changes in program: 2017-10-31, 2017-11-01
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 #include<avr/io.h>
 #include<stdio.h>
@@ -38,8 +38,8 @@ void uart_trans(unsigned char data);
 
 int main(void){
 	uart_int();
-	char towrite[200];
-	int i;
+	char towrite[100];
+	int i,j;
 	int checksum=0;
 	
 	//Faze 1: selection message
@@ -61,8 +61,39 @@ int main(void){
 	
 	//Faze 2: character input
 	checksum=0;
-	
-	
+	char line = uart_read();
+	if (line=='0'){
+		temp ="\rAO0001";
+		sprintf(towrite,"%s%02X\n", temp);
+		while(1){
+			for (i=8;i<32;i++)
+				towrite[i]=uart_read();
+		}
+	}
+	else if (line=='1'){
+		temp ="\rAO0001";
+		sprintf(towrite,"%s%02X\n", temp);
+		while(1){
+			for (i=8;i<32;i++)
+				towrite[i]=" ";
+			for (i=32;i<49;i++)
+				towrite[i]=uart_read();
+		}
+	}
+	else if (line==2){
+		temp ="\rBO0001";
+		sprintf(towrite,"%s%02X\n", temp);
+		while(1){
+			for (i=8;i<32;i++)
+				towrite[i]=uart_read();
+		}
+	}		
+			
+	for (i=0; i<(sizeof(towrite)+1);i++)
+		checksum+=towrite[i];
+	checksum = checksum%256;
+	sprintf(towrite,"%s%02X\n", checksum);
+			
 	//default broadcasting command
 	temp ="\rZD0013C\n";
 	for(i=0;i<9;i++){
@@ -81,7 +112,8 @@ void uart_trans(unsigned char data){
 	while(!(UCSR1A & (1<<UDRE1)));
 	UDR1 = data;
 }
-	    
+// read function	    
 char uart_read(void){
-	
+	while(!(UCSR1A & (1<<RXC1)));
+	return UDR1;	
 }
